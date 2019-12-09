befb816147f4de96933dc15ccf6772067fe76510
Savion <saviondeavault@icloud.com>
2019-12-09 11:56:43 -0500

    Initial Commit

---
+++ ScavengerHuntv2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScavengerHuntv2
+//
+//  Created by Savion DeaVault on 12/9/19.
+//  Copyright © 2019 Savion DeaVault. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
